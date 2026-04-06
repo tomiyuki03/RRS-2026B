beaf09a69eca8f46ebcbb5be22656d959b1cdd36
k23109 <k23109kk@aitech.ac.jp>
2026-04-07 01:15:56 +0900

    road_detectorを優先度計算方式に移行
    road_detectorのタスク割り当てを優先度計算基準に変更
    
    距離と優先道路であるかから計算
    
    重みは要変更

--- agent/src/agent/module/complex/road_detector.py
+++ agent/src/agent/module/complex/road_detector.py
@@ -14,6 +14,9 @@ from adf_core_python.core.component.module.algorithm.path_planning import (
 )
 from adf_core_python.core.component.module.complex.road_detector import RoadDetector
 
+# 評価計算：重みまとめ
+_WEIGHT_DISTANCE = 10.0
+_WEIGHT_PRIORITY_ROAD = 0.5
 
 class RoadDetector(RoadDetector):
   def __init__(
@@ -105,8 +108,10 @@ class RoadDetector(RoadDetector):
       # 現在位置はターゲットであるか？
       if self._agent_info.get_position_entity_id() == self._result:#ここ()修正
         entity = self._world_info.get_entity(self._result)
+        # ターゲットは建物なら，ターゲットから外す
         if isinstance(entity, Building):
           self._result = None
+        # ターゲットが道なら道ならば瓦礫が存在しない場合のみターゲットから外し，かつ候補集合からは除外
         elif isinstance(entity, Road):
           road = entity
           if road.get_blockades() == []:
@@ -116,41 +121,95 @@ class RoadDetector(RoadDetector):
     return self
 
   def calculate(self) -> RoadDetector:
+    # ターゲットは存在しているか
     if self._result is None:
+      # 現在地の所得
       position_entity_id = self._agent_info.get_position_entity_id()
+      # 現在地の所得確認
       if position_entity_id is None:
         return self
+      
+      # 現在の位置はターゲット候補に含まれる位置か
       if position_entity_id in self._target_areas:
+        # そうならターゲットに設定
         self._result = position_entity_id
         return self
+      
+      # 優先道路のなかですでに候補道路から外れているものを除去する
       remove_list = []
       for entity_id in self._priority_roads:
         if entity_id not in self._target_areas:
           remove_list.append(entity_id)
-
       self._priority_roads = self._priority_roads - set(remove_list)
-      if len(self._priority_roads) > 0:
-        agent_position = self._agent_info.get_position_entity_id()
-        if agent_position is None:
-          return self
-        _nearest_target_area = agent_position
-        _nearest_distance = float("inf")
-        for target_area in self._target_areas:
-          if (
-            self._world_info.get_distance(agent_position, target_area)
-            < _nearest_distance
-          ):
-            _nearest_target_area = target_area
-            _nearest_distance = self._world_info.get_distance(
-              agent_position, target_area
-            )
-        path: list[EntityID] = self._path_planning.get_path(
-          agent_position, _nearest_target_area
-        )
-        if path is not None and len(path) > 0:
-          self._result = path[-1]
 
+      # # 優先道路候補があるか
+      # if len(self._priority_roads) > 0:
+      #   # 現在位置の取得
+      #   agent_position = self._agent_info.get_position_entity_id()
+      #   # 現在位置の取得確認
+      #   if agent_position is None:
+      #     return self
+        
+      #   # 候補集合の中から最も近いものをターゲットとして決定
+      #   _nearest_target_area = agent_position
+      #   _nearest_distance = float("inf")
+      #   for target_area in self._target_areas:
+      #     if (
+      #       self._world_info.get_distance(agent_position, target_area)
+      #       < _nearest_distance
+      #     ):
+      #       _nearest_target_area = target_area
+      #       _nearest_distance = self._world_info.get_distance(
+      #         agent_position, target_area
+      #       )
+
+      #現在位置の取得
+      agent_position = self._agent_info.get_position_entity_id()
+      # 現在位置の取得確認
+      if agent_position is None:
+        return self
+      
+      # 評価値が最大のものをターゲットとして指定
+      _highest_target_area = agent_position
+      _highest_evaluate = float("-inf")
+
+      for target_area in self._target_areas:
+        _evaluate_target_area = self._evaluate(target_area)
+        if(_evaluate_target_area > _highest_evaluate):
+            _highest_evaluate = _evaluate_target_area
+            _highest_target_area = target_area
+      self._logger.info(f"targetarea = {_highest_target_area}")
+      # 経路探索
+      path: list[EntityID] = self._path_planning.get_path(
+        agent_position, _highest_target_area
+      )
+      # 経路が存在するか確認
+      if path is not None and len(path) > 0:
+        self._result = path[-1]
+    
     return self
+  
+  # 評価値計算
+  def _evaluate(self,target_area :EntityID) -> float:
+    return (
+      self._score_distance(target_area) * _WEIGHT_DISTANCE
+      + self._score_priority_road(target_area) * _WEIGHT_PRIORITY_ROAD
+    )
+  
+
+  # 距離スコア：距離+1の逆数を返す(0除算対策)
+  def _score_distance(self,target_area :EntityID) -> float:
+    #現在位置の取得
+    agent_position = self._agent_info.get_position_entity_id()
+    # 現在位置の取得確認
+    if agent_position is None:
+      return float("-inf")
+    
+    return  1/(self._world_info.get_distance(agent_position, target_area)+1)
+  
+  # 優先道路スコア：優先道路に含まれているか
+  def _score_priority_road(self,target_area:EntityID) -> float:
+    return 1.0 if target_area in self._priority_roads else 0.0
 
   def get_target_entity_id(self) -> Optional[EntityID]:
     return self._result
